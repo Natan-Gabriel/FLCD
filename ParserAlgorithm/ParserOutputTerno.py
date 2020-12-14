d2799ae092dca94c1147b447d2a869a730349d3d
ternovandarius <52597957+ternovandarius@users.noreply.github.com>
2020-12-14 10:36:15 +0200

    Better implementation (?)

--- ParserAlgorithm/ParserOutputTerno.py
+++ ParserAlgorithm/ParserOutputTerno.py
@@ -10,11 +10,11 @@ class Node:
     def __str__(self):
         msg = "Node nr " + str(self.index) + " with value " + str(self.val)
         if self.parent:
-            msg += ", parent " + str(self.parent.val)
+            msg += ", parent " + str(self.parent.index)
         else:
             msg += ", no parent"
         if self.sibling:
-            msg += " and sibling " + str(self.sibling.val)
+            msg += " and sibling " + str(self.sibling.index)
         else:
             msg += " and no sibling. "
         return msg
@@ -29,24 +29,42 @@ class ParserOutput:
     def build(self, workingStack):
         self.root = Node(self.index, workingStack[0], None, None)
         self.nodes.append(self.root)
+        workingStack.pop(0)
+        times_skipped = {}
+        for elem in workingStack:
+            times_skipped[elem] = 0
         pending_prods = [(self.root, copy.deepcopy(self.grammar.getProductionsForAGivenNonTerminal(self.root.val)[0]))]
-        while self.index < len(workingStack):
-            splits = workingStack[self.index].split("_")
-            if splits[0] in self.grammar.getNonTerminals():
-                prod_nr = int(splits[1])
-                new_node = Node(self.index+1, workingStack[self.index], pending_prods[-1][0], self.getSibling(pending_prods[-1][0]))
-                self.nodes.append(new_node)
-                pending_prods[-1][1].remove(splits[0])
-                if len(pending_prods[-1][1]) == 0:
-                    pending_prods.pop()
-                pending_prods.append((new_node, copy.deepcopy(self.grammar.getProductionsForAGivenNonTerminal(splits[0])[prod_nr])))
-            else:
-                new_node = Node(self.index+1, workingStack[self.index], pending_prods[-1][0], self.getSibling(pending_prods[-1][0]))
-                self.nodes.append(new_node)
-                pending_prods[-1][1].remove(workingStack[self.index])
-                if len(pending_prods[-1][1]) == 0:
-                    pending_prods.pop()
-            self.index+=1
+        while len(pending_prods) > 0:
+            for i in workingStack:
+                splits = i.split("_")
+                if splits[0] in pending_prods[0][1]:
+                    counter = 0
+                    for j in pending_prods[1:]:
+                        if splits[0] in j[1]:
+                            counter+=1
+                    if counter == times_skipped[i]:
+                        if splits[0] in self.grammar.getNonTerminals():
+                            prod_nr = int(splits[1])
+                            new_node = Node(self.index+1, i, pending_prods[0][0], self.getSibling(pending_prods[0][0]))
+                            self.nodes.append(new_node)
+                            pending_prods[0][1].remove(splits[0])
+                            if len(pending_prods[0][1]) == 0:
+                                pending_prods.pop(0)
+                            pending_prods.append((new_node, copy.deepcopy(self.grammar.getProductionsForAGivenNonTerminal(splits[0])[prod_nr])))
+                        else:
+                            new_node = Node(self.index+1, i, pending_prods[0][0], self.getSibling(pending_prods[0][0]))
+                            self.nodes.append(new_node)
+                            pending_prods[0][1].remove(i)
+                            if len(pending_prods[0][1]) == 0:
+                                pending_prods.pop(0)
+                        self.index+=1
+                        if times_skipped[i]>0:
+                            times_skipped[i]-=1
+                        workingStack.remove(i)
+                        break
+                    else:
+                        times_skipped[i]+=1
+
 
     def getSibling(self, parent):
         siblings = []
